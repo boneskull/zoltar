885606fff7a4ac437857b7f49c78e0e709af2691
Christopher Hiller <chiller@badwing.com>
2013-08-03 20:20:49 -0700

    scaffolding

---
+++ app/scripts/controllers/main.js
@@ -0,0 +1,10 @@
+'use strict';
+
+angular.module('zoltarApp')
+  .controller('MainCtrl', function ($scope) {
+    $scope.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
