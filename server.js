881c3d683a02006e1da848250142e5afb7a18ae2
Christopher Hiller <chiller@badwing.com>
2013-08-13 00:16:18 -0700

    port problem?

--- server.js
+++ server.js
@@ -71,7 +71,7 @@ require('./bootstrap')();
 
 app.http().io();
 require('./routing')(app);
-app.listen(app.get('port'), function () {
+app.listen(process.env.PORT || 3000, function () {
     console.log('Express server listening on port ' + app.get('port'));
 });
 
